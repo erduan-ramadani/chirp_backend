89110bf85cb3e3e9aa642ca68520d241c37e5b87
erduan <malondamatuidi@gmail.com>
2026-02-18 15:00:19 +0100

    Added build-logic module, cleaning

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,15 @@
+import org.gradle.kotlin.dsl.maven
+import org.gradle.kotlin.dsl.mavenCentral
+import org.gradle.kotlin.dsl.repositories
+
+pluginManagement {
+    includeBuild("build-logic")
+    repositories {
+        mavenCentral()
+        maven { url = uri("https://repo.spring.io/milestone" )}
+    }
+}
+
 rootProject.name = "chirp"
 
 include("app")
--- user/build.gradle.kts
+++ user/build.gradle.kts
@@ -12,7 +12,6 @@ version = "unspecified"
 repositories {
     mavenCentral()
     maven { url = uri("https://repo.spring.io/milestone" )}
-    maven { url = uri("https://repo.spring.io/snapshot") }
 }
 
 dependencies {

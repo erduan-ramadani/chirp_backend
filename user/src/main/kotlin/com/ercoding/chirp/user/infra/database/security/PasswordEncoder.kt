2b6c9078b97704275f414e922f81ff4f4c6126da
erduan <malondamatuidi@gmail.com>
2026-02-19 00:44:05 +0100

    Created Password encoder with BCrypt

---
+++ user/src/main/kotlin/com/ercoding/chirp/user/infra/database/security/PasswordEncoder.kt
@@ -0,0 +1,14 @@
+package com.ercoding.chirp.user.infra.database.security
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+
+class PasswordEncoder {
+
+    private val bcrypt = BCryptPasswordEncoder()
+
+    fun encode(rawPassword: String): String? = bcrypt.encode(rawPassword)
+
+    fun matches(rawPassword: String, hashedPassword: String): Boolean {
+        return bcrypt.matches(rawPassword, hashedPassword)
+    }
+}

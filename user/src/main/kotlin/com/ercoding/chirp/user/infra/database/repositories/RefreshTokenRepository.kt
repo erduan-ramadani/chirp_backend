22b02965e088f35f5d5c77f58916c24393ff419b
erduan <malondamatuidi@gmail.com>
2026-02-20 10:55:49 +0100

    Created Refreshtoken Entity and Repository

---
+++ user/src/main/kotlin/com/ercoding/chirp/user/infra/database/repositories/RefreshTokenRepository.kt
@@ -0,0 +1,11 @@
+package com.ercoding.chirp.user.infra.database.repositories
+
+import com.ercoding.chirp.user.domain.model.UserId
+import com.ercoding.chirp.user.infra.database.entities.RefreshTokenEntity
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface RefreshTokenRepository : JpaRepository<RefreshTokenEntity, Long> {
+    fun findByUserIdAndHashedToken(userId: UserId, hashedToken: String): RefreshTokenEntity?
+    fun deleteByUserIdAndHashedToken(userId: UserId, hashedToken: String)
+    fun deleteByUserId(userId: UserId)
+}

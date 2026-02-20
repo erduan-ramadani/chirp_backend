3a668758508c23746aebd74d97a5dfcd750f4755
erduan <malondamatuidi@gmail.com>
2026-02-20 15:22:57 +0100

    Implemented email verification service

--- user/src/main/kotlin/com/ercoding/chirp/user/infra/database/repositories/EmailVerificationTokenRepository.kt
+++ user/src/main/kotlin/com/ercoding/chirp/user/infra/database/repositories/EmailVerificationTokenRepository.kt
@@ -5,7 +5,7 @@ import com.ercoding.chirp.user.infra.database.entities.UserEntity
 import org.springframework.data.jpa.repository.JpaRepository
 import java.time.Instant
 
-interface EmailVerificationRepository : JpaRepository<EmailVerificationTokenEntity, Long> {
+interface EmailVerificationTokenRepository : JpaRepository<EmailVerificationTokenEntity, Long> {
     fun findByToken(token: String): EmailVerificationTokenEntity?
     fun deleteByExpiresAtLessThan(now: Instant)
     fun findByUserAndUsedAtIsNull(user: UserEntity): List<EmailVerificationTokenEntity>

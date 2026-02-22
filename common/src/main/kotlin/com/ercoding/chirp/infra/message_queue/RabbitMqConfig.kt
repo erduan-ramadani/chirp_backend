339d77fa5d1584881b6d03e4632d8c9e00ade012
erduan <malondamatuidi@gmail.com>
2026-02-22 10:43:08 +0100

    Setup RabbitMq config

---
+++ common/src/main/kotlin/com/ercoding/chirp/infra/message_queue/RabbitMqConfig.kt
@@ -0,0 +1,77 @@
+package com.ercoding.chirp.infra.message_queue
+
+import com.ercoding.chirp.domain.events.ChirpEvent
+import com.ercoding.chirp.domain.events.user.UserEventConstants
+import org.springframework.amqp.core.Binding
+import org.springframework.amqp.core.BindingBuilder
+import org.springframework.amqp.core.Queue
+import org.springframework.amqp.core.TopicExchange
+import org.springframework.amqp.rabbit.connection.ConnectionFactory
+import org.springframework.amqp.rabbit.core.RabbitTemplate
+import org.springframework.amqp.support.converter.JacksonJavaTypeMapper
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.transaction.annotation.EnableTransactionManagement
+import tools.jackson.databind.DefaultTyping
+import tools.jackson.databind.json.JsonMapper
+import tools.jackson.databind.jsontype.BasicPolymorphicTypeValidator
+import tools.jackson.module.kotlin.kotlinModule
+
+@Configuration
+@EnableTransactionManagement
+class RabbitMqConfig {
+
+    @Bean
+    fun messageConverter(): JacksonJsonMessageConverter {
+        val polymorphicTypeValidator = BasicPolymorphicTypeValidator.builder()
+            .allowIfBaseType(ChirpEvent::class.java)
+            .allowIfSubType("java.util.") // allow java lists
+            .allowIfSubType("kotlin.collections.") // Kotlin collections
+            .build()
+
+        val objectMapper = JsonMapper.builder()
+            .addModule(kotlinModule())
+            .polymorphicTypeValidator(polymorphicTypeValidator)
+            .activateDefaultTyping(polymorphicTypeValidator, DefaultTyping.NON_FINAL)
+            .build()
+
+        return JacksonJsonMessageConverter(objectMapper).apply {
+            typePrecedence = JacksonJavaTypeMapper.TypePrecedence.TYPE_ID
+        }
+    }
+
+    @Bean
+    fun rabbitTemplate(
+        connectionFactory: ConnectionFactory,
+        messageConverter: JacksonJsonMessageConverter
+    ): RabbitTemplate {
+        return RabbitTemplate(connectionFactory).apply {
+            this.messageConverter = messageConverter
+        }
+    }
+
+    @Bean
+    fun userExchange() = TopicExchange(
+        UserEventConstants.USER_EXCHANGE,
+        true,
+        false
+    )
+
+    @Bean
+    fun notificationUserEventsQueue() = Queue(
+        MessageQueues.NOTIFICATION_USER_EVENTS,
+        true
+    )
+
+    @Bean
+    fun notificationUserEventsBinding(
+        notificationUserEventsQueue: Queue,
+        userExchange: TopicExchange
+    ): Binding {
+        return BindingBuilder
+            .bind(notificationUserEventsQueue)
+            .to(userExchange)
+            .with("user.*")
+    }
+}

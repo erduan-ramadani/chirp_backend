915d016d1a31820a29c9fd03a624ba877c6621bf
erduan <malondamatuidi@gmail.com>
2026-02-27 11:46:38 +0100

    Implement DeviceToken

---
+++ notification/src/main/kotlin/com/ercoding/chirp/infra/database/DeviceTokenRepository.kt
@@ -0,0 +1,10 @@
+package com.ercoding.chirp.infra.database
+
+import com.ercoding.chirp.domain.type.UserId
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface DeviceTokenRepository : JpaRepository<DeviceTokenEntity, Long> {
+    fun findByUserIdIn(userIds: List<UserId>): List<DeviceTokenEntity>
+    fun findByToken(token: String): DeviceTokenEntity?
+    fun deleteByToken(token: String)
+}
